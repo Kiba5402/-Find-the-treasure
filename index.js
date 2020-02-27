d4bb02c45449306c6f05663b69ba9a5ade9aab13
Cristian R <cristian5402@gmail.com>
2020-02-27 11:49:30 -0500

    nueva distribucion

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const morgan = require('morgan');
 const router = require('./router/router');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 //configuracion
 app.set('views', './views/html');
